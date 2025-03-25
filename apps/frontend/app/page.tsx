ea7c4b09d5338c19aa0aeb1144f23359e40fe7f2
victorbuikem <chibuikemkalu466@gmail.com>
2025-03-25 07:18:40 +0100

    chore: update dependencies in package.json, enhance Tailwind CSS configuration, and implement Tabs component in Home page

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,10 +1,19 @@
-import { Test } from '@/components/test'
+import { Test } from '@/components/test';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to MesaChain</h1>
       <Test />
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
     </main>
   );
 } 
