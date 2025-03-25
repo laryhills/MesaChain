9b92ef5e999bd8513dfc137f3c2897d3a36f84f3
victorbuikem <chibuikemkalu466@gmail.com>
2025-03-25 06:36:11 +0100

    chore: update package.json dependencies and add Test component to Home page

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import { Test } from '@/components/test'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to MesaChain</h1>
+      <Test />
     </main>
   );
 } 
