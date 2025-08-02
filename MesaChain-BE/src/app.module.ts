de632405c735b26b18a06ef03677541e223e2111
Josue19-08 <josuemarin2009@hotmail.com>
2025-08-02 16:33:03 -0600

    refactor: update app module to use SharedModule instead of PrismaModule

--- MesaChain-BE/src/app.module.ts
+++ MesaChain-BE/src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from "@nestjs/common";
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
-import { PrismaModule } from "./prisma/prisma.module";
+import { SharedModule } from "./shared/shared.module";
 import { ConfigModule } from "./config/config.module";
 import { OrdersModule } from "./orders/orders.module";
 import { MenuModule } from "./menu/menu.module";
 import { ReservationsModule } from "./reservations/reservations.module";
 import { HealthModule } from './health/health.module';
 import { MetricsModule } from './metrics/metrics.module';
+import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 @Module({
   imports: [
     ConfigModule,
-    PrismaModule,
+    SharedModule,
     AuthModule,
     UsersModule,
     OrdersModule,
@@ -24,6 +26,12 @@ import { MetricsModule } from './metrics/metrics.module';
     MetricsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule { }
