5e2527a2bb063613c110535002e47b9e5fbfc3fe
Josue19-08 <josuemarin2009@hotmail.com>
2025-08-02 16:33:09 -0600

    refactor: update PrismaService import in JWT strategy

--- MesaChain-BE/src/auth/strategies/jwt.strategy.ts
+++ MesaChain-BE/src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,7 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../shared/prisma.service';
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from "../../prisma/prisma.service";
 import { ConfigService } from "@nestjs/config";
 
 @Injectable()
